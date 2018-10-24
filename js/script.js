70d1a5dbea7b3faa0086114dea4d9ca85a786c8d
Thienan Nguyen <thienan.nguyen@ousd.us>
2018-10-24 22:38:17 +0000

    Added images, some jquery to make the code run when images are clicked

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@
 var losses = 0;
 var wins = 0;
 var times = 0;
+
+
 $("#shoot").click(function(){
    $("#userChoice").text($("#input").val().toLowerCase()); 
    if ($("#input").val().toLowerCase() == "rock"){
@@ -25,7 +27,7 @@ $("#shoot").click(function(){
        alert("You played Paper. You Lose!");
        losses = losses + 1;
    } else {
-    var x = Math.floor(Math.random()*3)
+    var x = Math.floor(Math.random()*3);
     if (x == 0){
             $("#computerChoice").text("rock");
     } else if (x==1){
@@ -34,14 +36,52 @@ $("#shoot").click(function(){
             $("#computerChoice").text("paper");
     }
        $("#result").text("You Cheated. You Lose.");
-       alert("You Cheated. You Lose.")
+       alert("You Cheated. You Lose.");
        losses = losses + 1;
    }
-   times = times + 1
+   times = times + 1;
    $("#totaltimes").text(times);
    $("#wins").text(wins);
    $("#losses").text(losses);
    
 });
+
+
 // DOCUMENT READY FUNCTION BELOW
 
+
+$("#rock").click(function(){
+    $("#userChoice").text("rock");
+    $("#computerChoice").text("paper");
+    $("#result").text("You Lose.");
+    alert("You played rock. You Lose!");
+    losses = losses + 1;
+    times = times + 1;
+    $("#totaltimes").text(times);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+});
+
+$("#scissors").click(function(){
+    $("#userChoice").text("scissors");
+    $("#computerChoice").text("rock");
+    $("#result").text("You Lose.");
+    alert("You played scissors. You Lose!");
+    losses = losses + 1;
+    times = times + 1;
+    $("#totaltimes").text(times);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+});
+
+$("#paper").click(function(){
+    $("#userChoice").text("paper");
+    $("#computerChoice").text("scissors");
+    $("#result").text("You Lose.");
+    alert("You played paper. You Lose!");
+    losses = losses + 1;
+    times = times + 1;
+    $("#totaltimes").text(times);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+});
